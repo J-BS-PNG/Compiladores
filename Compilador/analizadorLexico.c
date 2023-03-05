e81d3c13db16035d8d16df1a0a8d5007e49abbb1
globv28 <glori2803@gmail.com>
2023-03-04 20:01:22 -0600

    avance analizador lexico 1

--- Compilador/analizadorLexico.c
+++ Compilador/analizadorLexico.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
-//#include "Diccionario/Diccionario.h"
-
-struct tipo {
-    char componente_lexico[100];
-    char lexema[10];
-}; 
+#include <stdlib.h>
 
 
+
+int contiene(char *arr[], char componenteLexico[]){
+    //printf(componenteLexico);
+    for (int i = 0; i < sizeof(arr); i++){
+        printf(arr[i]);
+        /*if(arr[i] == componenteLexico){
+            return 1; //true
+        }*/
+    }
+    return 0; //false
+};
 
 
 int main(){
@@ -15,12 +21,19 @@ int main(){
     printf("Igrese la operacion");
     scanf("%s", comando);
     printf( "%s\n", comando);
+
     /*Crear diccionario*/
     //Diccionario* operadores = diccionario_nuevo();
 
-    /*Digitos*/
+    /*Arrays de las llaves aka los */
+    char *DIGITOS[1][10] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    char *OPERADORES[1][6] = {"+", "-", "*", "/", "^", "="};
+    char *PUNTUACION[1][2] = {"(", ")"};
+    char *IDENTIFICADORES[4][2] = {"calc", "$"}; //tome la decisión administrativa de que  vamos a usar el $ para identificar las variables
     
 
+   //printf(strlen(comando));
+    
     /*Operaciones*/
     /*struct tipo sumar = {"op_sumar", "+"};
     struct tipo restar = {"op_restar", "-"};
